a94b6ce283ab90a527635f41b4c1b14925ddd553
Emily Coales <emilycoales@msn.com>
2018-03-10 19:02:19 +0000

    Graphs

--- script1.py
+++ script1.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 @app.route('/')
@@ -11,3 +12,18 @@ def about():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+#r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
+#r.status_code
+#200
+#r.headers['content-type']
+#'application/json; charset=utf8'
+#r.encoding
+#'utf-8'
+# r.text
+#u'{"type":"User"...'
+#r.json()
+#{u'private_gists': 419, u'total_private_repos': 77, ...}
+
+#GET https://management.azure.com/subscriptions/{subscriptionId}/resourcegroups/{resourceGroupName}?api-version=2017-05-10
